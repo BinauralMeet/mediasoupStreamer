1d532175602a05bfb9f0f02091372e3153c9591c
Shoichi Hasegawa <hasevr@gmail.com>
2022-10-03 22:25:05 +0900

    fix: announce ip

--- server/src/config.js
+++ server/src/config.js
@@ -53,14 +53,14 @@ module.exports = Object.freeze({
     ]
   },
   webRtcTransport: {
-    listenIps: [ { ip: '0.0.0.0', announcedIp: '172.27.0.1' } ], // TODO: Change announcedIp to your external IP or domain name
+    listenIps: [ { ip: '0.0.0.0', announcedIp: 'binaural.me' } ], // TODO: Change announcedIp to your external IP or domain name
     enableUdp: true,
     enableTcp: true,
     preferUdp: true,
     maxIncomingBitrate: 1500000
   },
   plainTransport: {
-    listenIp: { ip: '0.0.0.0', announcedIp: '172.27.0.1' }, // TODO: Change announcedIp to your external IP or domain name
+    listenIp: { ip: '0.0.0.0', announcedIp: 'binaural.me' }, // TODO: Change announcedIp to your external IP or domain name
     rtcpMux: true,
     comedia: false
   }
